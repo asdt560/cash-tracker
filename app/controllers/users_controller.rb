ed70c31fe68cc367c94a54a2bb106d9fe0294e87
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-02-13 17:45:03 -0300

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
   load_and_authorize_resource except: :create
-  def index
-  end
+  def index; end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,11 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     user ||= ::User.new
-    
+
     return unless user.present?
+
     can :read, :all
   end
 end
