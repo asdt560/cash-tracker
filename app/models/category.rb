295f56b855683104699fb0fc23d873bb7a888163
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-04-04 17:42:13 -0300

    Add image validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :payments
   validates :name, presence: true
   has_one_attached :image
+  validates :image, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
 
   def sum_of_payments
     payments = self.payments
