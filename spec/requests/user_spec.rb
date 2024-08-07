2628ce0e20b47e917a24fe688ff95af45af77675
JustoRago <103001263+asdt560@users.noreply.github.com>
2024-08-07 18:30:17 -0300

    Add test for user profile

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -17,4 +17,25 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('Cash Tracker')
     end
   end
+  describe 'GET users#show' do
+    before(:each) do
+      @user = User.new(name: 'nameofuser', email: 'asdt560@gmail.com', password: '6letters',
+                       encrypted_password: '$2a$12$192AtELpNZ0aZCfnSxs35umQYmbSn52FK8ML/vY.iZvDW4FvkvHn2')
+      @user.skip_confirmation!
+      @user.confirm
+      @user.save
+      sign_in @user
+      get '/users'
+    end
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      expect(response).to render_template('show')
+    end
+
+    it 'includes text' do
+      expect(response.body).to include(@user.name)
+    end
 end
